0df9308a5ffced78db282c05c2ab52f7e9aa9b0e
vhodges <victor.hodges@tutelkan.com>
2024-05-28 09:04:55 -0400

    chore(project): ✅ repair eslint and prettier deltas warnings 🐛

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import ComponentsApp from './src/ComponentsApp';
 
 AppRegistry.registerComponent(appName, () => ComponentsApp);
